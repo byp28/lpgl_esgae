f79d8f32dace0a605edc5a2affc7b3aee52d645c
byp28 <yannight28@gmail.com>
2024-04-24 17:17:46 +0100

    24/04 17

--- lpgl/admin.py
+++ lpgl/admin.py
@@ -4,5 +4,13 @@ from lpgl.models import Eleve,Livre
 
 # Register your models here.
 
+class EleveAdmin(admin.ModelAdmin):
+    list_display = ("name")
+
+class LivreAdmin(admin.ModelAdmin):
+    list_display = ("titre","quantite","eleve")
+    list_filter = ["titre"]
+    search_fields  = ["titre"]
+    
 admin.site.register(Eleve)
-admin.site.register(Livre)
+admin.site.register(Livre,LivreAdmin)
--- lpgl/models.py
+++ lpgl/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 
 class Eleve(models.Model):
-    name = models.CharField(max_length = 100, unique= True)
+    name = models.CharField(max_length = 100, unique= True, verbose_name ="name")
     
+    def __str__(self):
+        return self.name
     class Meta:
         verbose_name = "Eleve"
         verbose_name_plural = "Eleves"
         
 class Livre(models.Model):
-    titre = models.CharField(max_length = 100, unique= True)
-    quantite = models.IntegerField(default = 1)
-    eleve  = models.ForeignKey(Eleve, on_delete=models.CASCADE)
+    titre = models.CharField(max_length = 100, unique= True, verbose_name = "titre")
+    quantite = models.IntegerField(default = 1 , verbose_name = "quantite")
+    eleve  = models.ForeignKey(Eleve, on_delete=models.CASCADE,verbose_name = "Eleve")
     
+    def __str__(self):
+        return self.titre
     class Meta:
         verbose_name = "Livre"
         verbose_name_plural = "Livres"
